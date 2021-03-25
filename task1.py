9db1444840186c4eb19c4082f6a45a728c6b25d8
Divya Dharshini <77479678+DIVYADHARSHINI19@users.noreply.github.com>
2021-03-25 11:09:30 +0530

    AREA OF THE CIRCLE

---
+++ task1.py
@@ -0,0 +1,5 @@
+#Mycaptain Tasks
+from math import pi
+radius=float(input("Input the radius of the circle: "))
+area = (pi*radius*radius)
+print("The area of the circle with radius ",radius," is :",area)
